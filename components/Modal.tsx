69f892593db638b45bc15776710783c73c443a6e
unknown <ihorimberel@gmail.com>
2025-06-24 17:36:48 +0200

    Fixing Youtube videos issue

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -22,10 +22,7 @@ function Modal() {
         if(!movie) return 
         async function fetchMovie(){
             const data = await fetch(
-                `https:api.themoviedb.org/3/${
-                    movie?.media_type === 'tv' ? 'tv' : 'movie'
-                }/${movie?.id}?api_key=${process.env.NEXT_PUBLIC_API_KEY
-                    }&language=en-US&append_to_response=videos`
+                `https://api.themoviedb.org/3/${movie?.media_type === 'tv' ? 'tv' : 'movie'}/${movie?.id}?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US&append_to_response=videos`
             ).then((response) => response.json())
             .catch((err) => console.log(err.message))
 
